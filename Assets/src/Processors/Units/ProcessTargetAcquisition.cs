ae1e5425b63c657a71e71191d0582df162e7985c
Brian Flores <alumanix@gmail.com>
2021-04-14 04:07:54 -0500

    TP-38 fixed

--- Assets/src/Processors/Units/ProcessTargetAcquisition.cs
+++ Assets/src/Processors/Units/ProcessTargetAcquisition.cs
@@ -11,9 +11,9 @@ namespace ThePathfinder.Game
     {
         [ExcludeBy(GameComponent.Target, GameComponent.Passive)]
         private readonly Group<VisibleTargets> _entitiesWithoutTargets = default;
-        [ExcludeBy(GameComponent.Passive)]
-        private readonly Group<Target> _entitiesWithTargets = default;
-        
+
+        [ExcludeBy(GameComponent.Passive)] private readonly Group<Target> _entitiesWithTargets = default;
+
         public void Tick(float delta)
         {
             foreach (ent entity in _entitiesWithTargets)
@@ -30,17 +30,16 @@ namespace ThePathfinder.Game
                 float distanceToTarget = Vector3.Distance(target.Value.transform.position, entity.transform.position);
                 Draw.Line(entity.transform.position, target.Value.transform.position, Color.red);
                 float aggroRange = entity.AggroComponent().value;
-                if (distanceToTarget < aggroRange) continue;//all good, within attack range
-                Debug.Log("Entity "+entity+" lost target");
-                Debug.Log("Entity Range: "+aggroRange+" Distance to target: " + distanceToTarget);
+                if (distanceToTarget < aggroRange) continue; //all good, within attack range
+                Debug.Log("Entity " + entity + " lost target");
+                Debug.Log("Entity Range: " + aggroRange + " Distance to target: " + distanceToTarget);
                 entity.Remove<Target>();
             }
 
             foreach (ent entity in _entitiesWithoutTargets)
             {
-                                
                 VisibleTargets cVisibleTargets = entity.VisibleTargetsComponent();
-                Debug.Log(string.Concat("Scoring ",cVisibleTargets.value.length," Targets"));
+                Debug.Log(string.Concat("Scoring ", cVisibleTargets.value.length, " Targets"));
                 if (cVisibleTargets.value.length == 0) continue;
                 float closestDistance = float.PositiveInfinity;
                 ent closestTarget = default;
@@ -49,7 +48,7 @@ namespace ThePathfinder.Game
                     float distanceToTarget = math.distance(target.transform.position, entity.transform.position);
                     if (distanceToTarget > closestDistance) continue;
                     Debug.Log("Distance to target smaller than closest distance");
-                    if(distanceToTarget > entity.AggroComponent().value) continue;
+                    if (distanceToTarget > entity.AggroComponent().value) continue;
                     Debug.Log("Distance to target smaller than attack range, setting potential target");
                     closestTarget = target;
                     closestDistance = distanceToTarget;
@@ -66,7 +65,7 @@ namespace ThePathfinder.Game
         {
             foreach (ent entity in _entitiesWithTargets.removed)
             {
-                if(entity.Has<Target>()) entity.Remove<Target>();
+                if (entity.Has<Target>()) entity.Remove<Target>();
             }
         }
     }
